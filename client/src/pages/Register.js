427e90d7d05a97ac96c4e7a8900d86576d15cf0d
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-02-10 15:17:04 +0530

    protected route and logout function created

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, Input, message } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
@@ -20,7 +20,14 @@ const Register = () => {
             setLoading(false);
             message.error('something went wrong');
         }
-    }
+    };
+    // prevent for login user
+
+    useEffect(() => {
+        if (localStorage.getItem("user")) {
+            navigate("/");
+        }
+    }, [navigate]);
     return (
         <>
             <div className="register-page">
